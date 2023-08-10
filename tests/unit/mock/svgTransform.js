aae935ac4f74e70dcd7d7d1fadbddffb9a90c752
tranvannhan1911 <tranvannhan1911@gmail.com>
2023-08-11 01:16:30 +0700

    init

---
+++ tests/unit/mock/svgTransform.js
@@ -0,0 +1,8 @@
+// Copyright (C) 2021 Storj Labs, Inc.
+// See LICENSE for copying information.
+
+module.exports = {
+    process () {
+        return `module.exports = { render: function(){ return this._c("svg") } }`;
+    },
+};
